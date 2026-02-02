b8180add8d17f3ad273f6b90015795763795086c
Javier Coyoc <2024160552@ub.edu.bz>
2026-02-02 14:53:04 -0600

    Complete part 3: Object Literals

---
+++ exercise3.js
@@ -0,0 +1,18 @@
+const student = {
+ firstName: 'John',
+ lastName: 'Doe',
+ studentId: '12345',
+ courses: [],
+
+ getFullName() {
+    return this.firstName + " " + this.lastName;
+ },
+
+ enrollCourse(courseName) {
+    this.courses.push(courseName);
+ },
+
+ getCourseCount() {
+    return this.courses.length;
+ }
+};
